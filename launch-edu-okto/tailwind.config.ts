c407661f6d1143ea842786b14043de8b9ff41107
Hélène SCHOONHEERE <helene.reva.s@protonmail.com>
2023-04-26 16:25:14 +0200

    static homepage

--- launch-edu-okto/tailwind.config.ts
+++ launch-edu-okto/tailwind.config.ts
@@ -4,6 +4,10 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'Inter': ['inter', 'sans-serif'],
+    },
   },
   plugins: [],
+  
 } satisfies Config;
