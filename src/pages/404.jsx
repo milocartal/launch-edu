966d360f0696de2a87de1ab4a5e80b3ad741f134
Hélène SCHOONHEERE <helene.reva.s@protonmail.com>
2023-05-10 16:53:27 +0200

    404 start

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import {TbError404} from "react-icons/tb"
+import Header from "./components/header";
 
 export default function Custom404() {
     return (
@@ -8,10 +9,12 @@ export default function Custom404() {
       <Head>
         <title>ERROR 404</title>
       </Head>
-      <main className="flex flex-col h-screen w-full items-center justify-center gap-10">
-        <img src="https://media.discordapp.net/attachments/688793736620146689/915869475423813662/20210709_215217.gif" alt="Les erreurs 404, envie de crever" className="max-w-[50%]"/>
-        <h1>Et merde, une 404, j'en ai marre</h1>
-        <img src="https://media.tenor.com/7zghNYrwGgAAAAAC/kermit-the.gif" className="max-w-[10rem]"/>
+      <main className="flex flex-col h-screen w-full items-center justify-center bg-[#1A808C]">
+
+        <h4 className="text-[200px] font-Inter text-[#63AEAB]">404</h4>
+        <p className="text-[36px] font-Inter text-white">Aïe, on n'a pas encore ce cours :(</p>
+        <p className="text-base font-Inter text-white">Quelques thématiques qui pourraient vous intéresser</p>
       </main>
+      <Header />
       </>);
   }
