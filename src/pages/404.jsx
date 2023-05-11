69ee0e34199df56d672eb21088068af6ef64bb0b
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-05-11 13:38:32 +0200

    mege 404 & main

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,17 +1,39 @@
 import Head from "next/head";
-import {TbError404} from "react-icons/tb"
+import Header from "./components/header";
+import Techno from "./components/techno";
+
+
+import { api } from "~/utils/api";
+import { useEffect, useState } from "react";
 
 export default function Custom404() {
+
+    const { data: techs } = api.technologie.getAll.useQuery();
+    const [troisTechs, setTroisTechs] = useState(techs)
+
+    useEffect(() => {
+        setTroisTechs(techs?.slice(0, 3))
+    }, [techs]);
+
     return (
-      
-      <>
-      <Head>
-        <title>ERROR 404</title>
-      </Head>
-      <main className="flex flex-col h-screen w-full items-center justify-center gap-10">
-        <img src="https://media.discordapp.net/attachments/688793736620146689/915869475423813662/20210709_215217.gif" alt="Les erreurs 404, envie de crever" className="max-w-[50%]"/>
-        <h1>Et merde, une 404, j'en ai marre</h1>
-        <img src="https://media.tenor.com/7zghNYrwGgAAAAAC/kermit-the.gif" className="max-w-[10rem]"/>
-      </main>
-      </>);
-  }
+        <>
+            <Head>
+                <title>Erreur 404</title>
+            </Head>
+            <main className="flex flex-col min-h-screen w-full items-center justify-center bg-[#1A808C] ">
+
+                <h4 className="text-[200px] font-Inter text-[#63AEAB]">404</h4>
+                <p className="text-[36px] font-Inter text-white -mt-10">Aïe, tu t'es perdu :(</p>
+                <p className="text-base font-Inter text-white mb-16">Quelques thématiques qui pourraient vous intéresser</p>
+
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-3 items-center justify-center w-full gap-y-20 px-52 mb-16">
+                    {troisTechs && troisTechs.length > 0 && troisTechs.map((tech) => {
+                        return (
+                            <Techno data={tech} key={tech.id} />
+                        )
+                    })}
+                </div>
+            </main>
+            <Header selected={404} />
+        </>);
+}
