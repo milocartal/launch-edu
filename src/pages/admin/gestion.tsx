8b9291fb28d254b12a24cb2baba19825bf556805
Hélène SCHOONHEERE <helene.reva.s@protonmail.com>
2023-05-15 17:07:27 +0200

    dark mode update and other fixes

--- src/pages/admin/gestion.tsx
+++ src/pages/admin/gestion.tsx
@@ -7,7 +7,7 @@ import Header from "../components/header";
 import { prisma } from '~/server/db';
 import { Technologie, Formation, Lecon, Etape, Prisma, EtapeType } from '@prisma/client';
 import { SyntheticEvent, useState } from 'react';
-import { HiXMark } from 'react-icons/hi2';
+import { HiMagnifyingGlass, HiXMark } from 'react-icons/hi2';
 
 import Title from '~/pages/components/title';
 
@@ -105,7 +105,12 @@ const Gestionnaire: NextPage = () => {
                 <div className='flex w-full justify-between h-[100%] items-center pt-10'>
                     <section className="flex flex-col h-full w-[40%]">
                         <p className="mb-3">Les thématiques:</p>
-                        <input type='text' name="lecon tag" placeholder='Rechercher un tag' className="p-[1rem] rounded-t-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full" autoComplete="off" onChange={handleSearchTag} />
+                        <div className="p-2 flex flex-row justify-center rounded-t-lg bg-[#0E6073] dark:bg-[#05262E] w-full">
+                            <div className="bg-white dark:bg-[#041F25] flex flex-row justify-start items-center width w-96 h-12 px-8 rounded-full shadow-[inset_4px_5px_12px_4px_rgba(0,0,0,0.25)]">
+                                <HiMagnifyingGlass className="h-8 w-8 text-[#989898] dark:text-[#63AEAB]" />
+                                <input type='text' name="lecon tag" placeholder='Rechercher un tag' className="h-10 shadow-none w-full bg-transparent text-black dark:text-white ml-1" autoComplete="off" onChange={handleSearchTag}/>
+                            </div>
+                        </div>
                         <fieldset className="flex flex-col justify-between max-h-[80%] w-full shadow-xl shadow-black/30 rounded-b-lg">
                             <fieldset className="flex flex-col w-full" id="listTech">
                                 {techList as Technologie[] && techList && techList.length > 0 && techList.filter((techno) => { return techno.name.toLowerCase().includes(SearchTag.toLowerCase()) }).map((techno) => {
@@ -143,7 +148,12 @@ const Gestionnaire: NextPage = () => {
 
                     <section className="flex flex-col h-full w-[40%]">
                         <p className="mb-3">Les étapes de cours</p>
-                        <input type='text' name="lecon tag" placeholder='Rechercher un tag' className="p-[1rem] rounded-t-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full" autoComplete="off" onChange={handleSearchType} />
+                        <div className="p-2 flex flex-row justify-center rounded-t-lg bg-[#0E6073] dark:bg-[#05262E] w-full">
+                            <div className="bg-white dark:bg-[#041F25] flex flex-row justify-start items-center width w-96 h-12 px-8 rounded-full shadow-[inset_4px_5px_12px_4px_rgba(0,0,0,0.25)]">
+                                <HiMagnifyingGlass className="h-8 w-8 text-[#989898] dark:text-[#63AEAB]" />
+                                <input type='text' name="lecon tag" placeholder='Rechercher un tag' className="h-10 shadow-none w-full bg-transparent text-black dark:text-white ml-1" autoComplete="off" onChange={handleSearchType}/>
+                            </div>
+                        </div>
                         <fieldset className="flex flex-col justify-between max-h-[80%] w-full shadow-xl shadow-black/30 rounded-b-lg">
                             <fieldset className="flex flex-col w-full" id="listTech">
                                 {typeList as EtapeType[] && typeList && typeList.length > 0 && typeList.filter((type) => { return type.name.toLowerCase().includes(SearchType.toLowerCase()) }).map((type) => {
@@ -186,13 +196,13 @@ const Gestionnaire: NextPage = () => {
 
             {tab === "tech" &&
                 <div className="fixed w-full h-full bg-[#0E6073]/90 top-0 right-0 left-0 bottom-0 flex justify-center items-center">
-                    <form onSubmit={handlerAddTech} className="relative flex flex-col gap-5 justify-center items-center bg-white rounded-xl p-16 w-[30%]" method="POST">
+                    <form onSubmit={handlerAddTech} className="relative flex flex-col gap-5 justify-center items-center bg-white dark:bg-[#082F38] rounded-xl p-16 w-[30%]" method="POST">
                         <div onClick={() => { setTab("normal"); setLogo("") }} className="absolute top-3 right-4 rounded-full font-semibold  no-underline transition hover:text-red-500 hover:cursor-pointer">
                             <HiXMark className="text-[2rem] text-[#0e6073] hover:text-red-500" />
                         </div>
-                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] w-full"><label htmlFor="techName">Nouvelle thématique</label></h1>
-                        <input name="techName" id="techName" type="text" placeholder="Nom de la technologie" required className="p-[1rem] rounded-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full" autoComplete="off"></input>
-                        <input name="logoTech" id="logoTech" type="url" placeholder="URL du Logo" required className="p-[1rem] rounded-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full" autoComplete="off" onChange={(e) => setLogo(e.target.value)}></input>
+                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] dark:text-[#1A808C] w-full"><label htmlFor="techName">Nouvelle thématique</label></h1>
+                        <input name="techName" id="techName" type="text" placeholder="Nom de la technologie" required className="p-[1rem] rounded-lg bg-none dark:bg-[#041F25] shadow-[inset_4px_5px_12px_6px_rgba(0,0,0,0.25)] w-full" autoComplete="off"></input>
+                        <input name="logoTech" id="logoTech" type="url" placeholder="URL du Logo" required className="p-[1rem] rounded-lg bg-none dark:bg-[#041F25] shadow-[inset_4px_5px_12px_6px_rgba(0,0,0,0.25)] w-full" autoComplete="off" onChange={(e) => setLogo(e.target.value)}></input>
                         {urlLogo !== "" && urlLogo.length > 8 ? <img src={urlLogo} className="max-w-[6rem] max-h-[6rem] min-x-[0px] min-h-[0px]" /> : <></>}
                         <button className="rounded-full bg-[#0E6073] text-white px-10 py-3 font-semibold w-full no-underline transition hover:bg-[#0E6073]/20" type="submit">Ajouter</button>
 
@@ -202,24 +212,24 @@ const Gestionnaire: NextPage = () => {
 
             {tab === "type" &&
                 <div className="fixed w-full h-full bg-[#0E6073]/90 top-0 right-0 left-0 bottom-0 flex justify-center items-center">
-                    <form onSubmit={handlerAddType} className="relative flex flex-col gap-5 justify-center items-center bg-white rounded-xl p-16 w-[40%]" method="POST">
+                    <form onSubmit={handlerAddType} className="relative flex flex-col gap-5 justify-center items-center bg-white dark:bg-[#082F38] rounded-xl p-16 w-[40%]" method="POST">
                         <div onClick={() => { setTab("normal"); setLogo("") }} className="absolute top-3 right-4 rounded-full font-semibold  no-underline transition hover:text-red-500 hover:cursor-pointer">
                             <HiXMark className="text-[2rem] text-[#0e6073] hover:text-red-500" />
 
                         </div>
-                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] w-full"><label htmlFor="techName">Nouvelle étape de cours</label></h1>
-                        <input name="typeName" id="typeName" type="text" placeholder="Nom de l'étape de cours" className="p-[1rem] rounded-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full" autoComplete="off"></input>
+                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] dark:text-[#1A808C] w-full"><label htmlFor="techName">Nouvelle étape de cours</label></h1>
+                        <input name="typeName" id="typeName" type="text" placeholder="Nom de l'étape de cours" className="p-[1rem] rounded-lg bg-none dark:bg-[#041F25] shadow-[inset_4px_5px_12px_6px_rgba(0,0,0,0.25)] w-full" autoComplete="off"></input>
                         <button className="rounded-full bg-[#0E6073] text-white px-10 py-3 font-semibold w-full no-underline transition hover:bg-[#0E6073]/20" type="submit">Ajouter</button>
                     </form>
                 </div>}
 
             {tab === "techModif" &&
                 <div className="fixed w-full h-full bg-[#0E6073]/90 top-0 right-0 left-0 bottom-0 flex justify-center items-center">
-                    <form onSubmit={handlerModifTech} className="relative flex flex-col gap-5 justify-center items-center bg-white rounded-xl p-16 w-[30%]" method="POST">
+                    <form onSubmit={handlerModifTech} className="relative flex flex-col gap-5 justify-center items-center bg-white dark:bg-[#082F38] rounded-xl p-16 w-[30%]" method="POST">
                         <div onClick={() => { setTab("normal"); setTech(undefined) }} className="absolute top-3 right-4 rounded-full font-semibold  no-underline transition hover:text-red-500 hover:cursor-pointer">
                             <HiXMark className="text-[2rem] text-[#0e6073] hover:text-red-500" />
                         </div>
-                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] w-full"><label htmlFor="techName">Modifier thématique</label></h1>
+                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] dark:text-[#1A808C] w-full"><label htmlFor="techName">Modifier thématique</label></h1>
 
                         <input
                             name="techName"
@@ -227,7 +237,7 @@ const Gestionnaire: NextPage = () => {
                             type="text"
                             placeholder="Nom de la technologie"
                             required
-                            className="p-[1rem] rounded-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full"
+                            className="p-[1rem] rounded-lg bg-none dark:bg-[#041F25] shadow-[inset_4px_5px_12px_6px_rgba(0,0,0,0.25)] w-full"
                             autoComplete="off"
                             defaultValue={selectedTech?.name} />
 
@@ -237,7 +247,7 @@ const Gestionnaire: NextPage = () => {
                             type="url"
                             placeholder="URL du Logo"
                             required
-                            className="p-[1rem] rounded-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full"
+                            className="p-[1rem] rounded-lg bg-none dark:bg-[#041F25] shadow-[inset_4px_5px_12px_6px_rgba(0,0,0,0.25)] w-full"
                             autoComplete="off"
                             onChange={(e) => setLogo(e.target.value)}
                             defaultValue={selectedTech?.logo} />
@@ -251,18 +261,18 @@ const Gestionnaire: NextPage = () => {
 
             {tab === "typeModif" &&
                 <div className="fixed w-full h-full bg-[#0E6073]/90 top-0 right-0 left-0 bottom-0 flex justify-center items-center">
-                    <form onSubmit={handlerModifType} className="relative flex flex-col gap-5 justify-center items-center bg-white rounded-xl p-16 w-[40%]" method="POST">
+                    <form onSubmit={handlerModifType} className="relative flex flex-col gap-5 justify-center items-center bg-white dark:bg-[#082F38] rounded-xl p-16 w-[40%]" method="POST">
                         <div onClick={() => { setTab("normal");  setType(undefined)}} className="absolute top-3 right-4 rounded-full font-semibold  no-underline transition hover:text-red-500 hover:cursor-pointer">
                             <HiXMark className="text-[2rem] text-[#0e6073] hover:text-red-500" />
                         </div>
-                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] w-full"><label htmlFor="techName">Modifier type</label></h1>
+                        <h1 className="text-xl font-extrabold tracking-tight text-[#0e6073] dark:text-[#1A808C] w-full"><label htmlFor="techName">Modifier type</label></h1>
 
                         <input
                             name="typeName"
                             id="typeName"
                             type="text"
                             placeholder="Nom de l'étape de cours"
-                            className="p-[1rem] rounded-lg bg-none shadow-[inset_4px_4px_12px_4px_rgba(0,0,0,0.25)] w-full"
+                            className="p-[1rem] rounded-lg bg-none dark:bg-[#041F25] shadow-[inset_4px_5px_12px_6px_rgba(0,0,0,0.25)] w-full"
                             autoComplete="off"
                             defaultValue={selectedType?.name} />
 
