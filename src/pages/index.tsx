ea73ea8b32e6f06cb64e968506b3183d8f6e623e
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-05-16 17:06:33 +0200

    hello

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,7 +69,6 @@ const Home: NextPage = () => {
               <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] mb-12">
                 Des cours sur mesure
               </h1>
-              {/*<p className="text-white text-xl font-Inter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nulla nisl, placerat sed mauris vitae, vestibulum sollicitudin libero.</p>*/}
               <p className="text-white text-xl font-Inter w-10/12">Insérer une citation drôle parce qu'on est pas sérieux.</p>
             </div>
             <img src="/gigaKristen.png" className="h-[600px]" alt="" />
@@ -124,8 +123,6 @@ const Home: NextPage = () => {
               
             })}
           </div>
-
-          {/* <img src="https://media.discordapp.net/attachments/688793736620146689/915869475423813662/20210709_215217.gif" className="max-w-[12em]"></img> */}
 
         </div>
         <div className="w-full bg-[#0e6370] dark:bg-[#041F25] h-36 bottom-0 mt-12 flex flex-row px-48 justify-between items-center">
